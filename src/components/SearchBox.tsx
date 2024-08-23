6ea444ad98e0b3e80d7837d6fc55c4b511671b2b
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-08-23 14:49:34 -0600

    Boton para cerrar búsqueda

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,4 @@
-import { ArrowPathIcon } from "@heroicons/react/20/solid"
+import { ArrowPathIcon, XCircleIcon } from "@heroicons/react/20/solid"
 import { useAddCity } from "../hooks/useAddCity"
 import { useSearchCities } from "../hooks/useSearchCities"
 import { useAddedCities } from "../hooks/useAddedCities"
@@ -13,21 +13,25 @@ function SearchBox() {
   const [searchStr, setSearchStr] = useState('')
   const searchBoxRef = useRef<HTMLDivElement | null>(null)
 
+  /*
   window.addEventListener('click', (e) => {
     const mousex = e.clientX
     const mousey = e.clientX
-    const { x, y, width, height } = searchBoxRef.current?.getBoundingClientRect()
+    if (searchBoxRef.current) {
+      const { x, y, width, height } = searchBoxRef.current!.getBoundingClientRect()
 
-    if (mousex > x + width) {
-      setOpen(false)
-    } else if (mousex < x) {
-      setOpen(false)
-    } else if (mousey > y) {
-      setOpen(false)
-    } else if (mousey < y + height) {
-      setOpen(false)
+      if (mousex > x + width) {
+        setOpen(false)
+      } else if (mousex < x) {
+        setOpen(false)
+      } else if (mousey > y) {
+        setOpen(false)
+      } else if (mousey < y + height) {
+        setOpen(false)
+      }
     }
   })
+  */
 
   return (
     <div ref={searchBoxRef} className='absolute z-20'>
@@ -44,15 +48,21 @@ function SearchBox() {
         placeholder='Find place or timezone - Press ↲ (Enter)'
       />
       {open ?
-        <ul className='search-results-container max-h-80 p-4 rounded-xl overflow-y-scroll bg-white shadow-md'>
-          {!searching ? searchResults.filter(ca => !addedCities.some(cb => cb.city == ca.city)).map((city, index) => (
-            <li key={`city-${index}-${city.city_ascii}`}>
-              <button onClick={() => handleAdd(city)} className='w-full p-2 my-1 rounded-xl text-start hover:bg-slate-300'>
-                {city.city}, {city.country}
-              </button>
-            </li>
-          )) : <ArrowPathIcon className='animate-spin h-5 w-5 mx-auto text-slate-600'></ArrowPathIcon>}
-        </ul> : null}
+        <>
+          <button className='absolute' onClick={() => setOpen(false)}>
+            <XCircleIcon className='h-8 w-8 text-red-400'></XCircleIcon>
+          </button>
+          <ul className='search-results-container max-h-80 p-4 rounded-xl overflow-y-scroll bg-white shadow-md'>
+            {!searching ? searchResults.filter(ca => !addedCities.some(cb => cb.city == ca.city)).map((city, index) => (
+              <li key={`city-${index}-${city.city_ascii}`}>
+                <button onClick={() => handleAdd(city)} className='w-full p-2 my-1 rounded-xl text-start hover:bg-slate-300'>
+                  {city.city}, {city.country}
+                </button>
+              </li>
+            )) : <ArrowPathIcon className='animate-spin h-5 w-5 mx-auto text-slate-600'></ArrowPathIcon>}
+          </ul>
+        </>
+        : null}
     </div>
   )
 
